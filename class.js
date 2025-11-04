0fdd586395e520226e6737169cc490f46cada15b
DimasKampus <2411031@students.universitasmulia.ac.id>
2025-11-04 19:37:16 +0800

    Commit pertama

---
+++ class.js
@@ -0,0 +1,18 @@
+class Karyawan {
+    constructor(nsms, jabatan, gsji) {
+    this.nama = nama;
+    this.jabatan = jabatan;
+    this.gaji = gaji;
+  }
+
+info() {
+    return console.log(
+        `$(this.nama) bekerja di perusahaan dengan jabatan $(this.jabatan) dengan gaji ${this.gaji}`);
+    }
+}
+
+const karyawan = new karyawan("Dimas", "orang biasa", 100);
+karyawan.info();
+
+class
+    
